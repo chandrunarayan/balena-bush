baabbc8b04158792454da86fbec33561d4b9d171
Chandru Narayan <54154611+chandrunarayan@users.noreply.github.com>
2023-02-03 20:14:51 -0800

    Update entrypoint.sh

--- jupyter/entrypoint.sh
+++ jupyter/entrypoint.sh
@@ -9,5 +9,6 @@ cp -f /usr/src/app/*.py /data
 ln -s /data /work/notebooks/data
 
 libcamera-hello --list-cameras -n -v
+libcamera-still -n -o /data/start.jpg
 jupyter lab --notebook-dir=/work/notebooks --port=8081 --no-browser --ip=0.0.0.0 --allow-root
 sleep infinity
